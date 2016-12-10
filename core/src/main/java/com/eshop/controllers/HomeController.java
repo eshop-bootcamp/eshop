a7badcf2964f0f978c67607a55c636db3dd3928c
Akshay Bansod <akshaybansod90@gmail.com>
2016-12-10 17:03:55 +0530

    added HomeController changes

--- core/src/main/java/com/eshop/controllers/HomeController.java
+++ core/src/main/java/com/eshop/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.eshop.controller;
+package com.eshop.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
---
+++ core/src/test/java/com/eshop/controllers/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.eshop.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by akshaybansod on 12/10/16.
+ */
+public class HomeControllerTest {
+
+    @Test
+    public void testHelloWorld(){
+        assertTrue(true);
+    }
+}
