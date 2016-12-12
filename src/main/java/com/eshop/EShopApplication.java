caf42b5fb22a390baa4d3d3f852161f0c6a254e6
gkoundi <gkoundi@thoughtworks.com>
2016-12-12 21:05:37 +0530

    Ability to refer to different datasource for integration test. give commandline parameter --spring.profiles.active=test and change the integrationTest.sql for running the test

--- src/main/java/com/eshop/EShopApplication.java
+++ src/main/java/com/eshop/EShopApplication.java
@@ -2,8 +2,10 @@ package com.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.eshop.repositories")
 public class EShopApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/eshop/repositories/CategoryRepository.java
+++ src/main/java/com/eshop/repositories/CategoryRepository.java
@@ -2,9 +2,11 @@ package com.eshop.repositories;
 
 import com.eshop.model.Category;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by sappana on 12/12/2016.
  */
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 }
