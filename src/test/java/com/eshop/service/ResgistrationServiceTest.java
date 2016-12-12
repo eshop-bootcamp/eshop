a144ff46b2215c10625595a6fd6f5ce2f7c8d0c7
DhirajHimani <dhrjkmr538@gmail.com>
2016-12-12 20:39:21 +0530

    removed unused imports

--- src/test/java/com/eshop/service/ResgistrationServiceTest.java
+++ src/test/java/com/eshop/service/ResgistrationServiceTest.java
@@ -1,9 +1,6 @@
 package com.eshop.service;
 
-import com.eshop.controllers.AuthController;
 import com.eshop.models.User;
-import com.sun.tools.internal.ws.wsdl.document.http.HTTPConstants;
-import org.json.HTTP;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
