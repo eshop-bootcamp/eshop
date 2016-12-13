4fe655d7e1cfe9f74cd49ece5bda082e6aeef80c
gkoundi <gkoundi@in-gkoundi.local>
2016-12-13 15:37:00 +0530

    changed http port from 8442 to 8080

--- src/main/java/com/eshop/EShopApplication.java
+++ src/main/java/com/eshop/EShopApplication.java
@@ -1,11 +1,7 @@
 package com.eshop;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
