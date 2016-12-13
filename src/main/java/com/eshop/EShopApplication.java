a534963c65f5495a268e4741b20834a532e5da4a
gkoundi <gkoundi@in-gkoundi.local>
2016-12-13 15:34:58 +0530

    Added logic for redirection from http to https

--- src/main/java/com/eshop/EShopApplication.java
+++ src/main/java/com/eshop/EShopApplication.java
@@ -1,12 +1,17 @@
 package com.eshop;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.eshop.repositories")
 public class EShopApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(EShopApplication.class, args);
