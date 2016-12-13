bf8913185f9dffdac32ad469bf7b1563efbb3ba1
Priti Biyani <pritib@in-pritibiyani.local>
2016-12-13 22:12:59 +0530

    [US#03] | Changing User model and added necessary service need in UserRepo for authentication | Akshay + Priti

--- src/main/java/com/eshop/auth/config/AuthenticationConfiguration.java
+++ src/main/java/com/eshop/auth/config/AuthenticationConfiguration.java
@@ -1,44 +1,23 @@
 package com.eshop.auth.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
 @EnableWebSecurity
-
+@Configuration
 public class AuthenticationConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
-    Environment environment;
-
-    @Autowired private SecurityProperties securityProperties;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.portMapper()
-                .http(Integer.parseInt(environment.getProperty("server.http.port")))
-                .mapsTo(Integer.parseInt(environment.getProperty("server.port")));
-        if (securityProperties.isRequireSsl()) http.requiresChannel().anyRequest().requiresSecure();
-
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .anyRequest().fullyAuthenticated()
                 .and()
-                .httpBasic().and()
-//                .authorizeRequests().antMatchers("/user").permitAll()
-//                .and()
-                .csrf().disable()
-        ;
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("username").password("password").roles("USER");
-    }
 }
--- src/main/java/com/eshop/repositories/UserRepository.java
+++ src/main/java/com/eshop/repositories/UserRepository.java
@@ -2,10 +2,10 @@ package com.eshop.repositories;
 
 import com.eshop.model.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    public User findByUsername(String username);
 
 }
 
