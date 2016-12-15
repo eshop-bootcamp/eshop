1fee5fe9f7b7f7ac9158136f1a1937af5390a9a5
Jinto Jose <jintoppy@gmail.com>
2016-12-15 10:10:08 +0530

    Fixing Test failure for UserController and ignoring integration in unit test gradle test | Jinto&Srividay

--- src/main/java/com/eshop/services/CategoryService.java
+++ src/main/java/com/eshop/services/CategoryService.java
@@ -5,12 +5,11 @@ import com.eshop.model.Item;
 import com.eshop.repositories.CategoryRepository;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * Created by srividhya on 13/12/16.
- */
+@Service
 public class CategoryService {
 
     @Autowired
--- src/test/java/com/eshop/controllers/AuthControllerTest.java
+++ src/test/java/com/eshop/controllers/AuthControllerTest.java
@@ -5,12 +5,16 @@ import com.eshop.model.User;
 import com.eshop.repositories.UserRepository;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 
 public class AuthControllerTest {
 
